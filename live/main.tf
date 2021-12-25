8563336242e35d789ad5b61f36fc55ce299def6f
Rob Roos <robroos@git.daimler.com>
2021-12-25 17:49:50 +0100

    change trigger

--- live/main.tf
+++ live/main.tf
@@ -1,17 +1,19 @@
-module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "~>2.0"
-  name               = local.prefix
-  cidr               = local.vpc_cidr
-  private_subnets    = local.private_subnets
-  public_subnets     = local.public_subnets
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+# module "vpc" {
+#   source             = "terraform-aws-modules/vpc/aws"
+#   version            = "~>2.0"
+#   name               = local.prefix
+#   cidr               = local.vpc_cidr
+#   private_subnets    = local.private_subnets
+#   public_subnets     = local.public_subnets
+#   enable_nat_gateway = false
+#   enable_vpn_gateway = false
 
-  azs = local.availability_zones
+#   azs = local.availability_zones
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
+
+
 
 resource "aws_s3_bucket" "app" {
   acl = "public-read"
